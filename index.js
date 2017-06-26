f74185858644a3022ecd8c62b51146a9acc97f41
Falk Hoppe <falkhoppe81@gmail.com>
2017-06-26 15:54:34 +0200

    add variables to markdown

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const MarkdownIt = require('markdown-it')
 
+// NASTY_BUG: passing a module-wide nodeRef object, that gives access, to the
+//                     current node in progress. This nasty solution was chosen because of
+//                     the bad implementations for markdown variables.
+//                     But it "should" work properly because, the set and get run in no time
+//                     after another. But yes, ... this is the root of all evil.
+const nodeRef = {
+  node: null,
+
+  getNodeData () {
+    return this.node && this.node.data
+  },
+
+  setNodeData (node) {
+    this.node = node
+  }
+}
+
 class MarkdownConverter {
   constructor (engine) {
     this.converter = engine
@@ -7,6 +24,7 @@ class MarkdownConverter {
 
   init (leitmotiv) {
     leitmotiv.converters['md'] = (node) => {
+      nodeRef.setNodeData(node)
       node.content = this.converter.render(node.content.toString(), node.data)
       return node
     }
@@ -20,5 +38,10 @@ class MarkdownConverter {
   }
 }
 
+const mdVariables = require('mdvariables')
 const engine = new MarkdownIt()
+// Adding variables for all users, because the implementation is rather complicated.
+// see NASTY_BUG comment above
+  .use(mdVariables(nodeRef.getNodeData.bind(nodeRef)))
+
 module.exports = new MarkdownConverter(engine)
